df8bedcafa84fa9a2174d826f35290464577fc0d
Johannes Zarre <zarre.johannes@gmail.com>
2020-11-13 20:41:31 +0100

    added category pages

--- dvds.php
+++ dvds.php
@@ -4,17 +4,16 @@
 
 <html>
 <head>
-   <title>BIG-EFFING-LIBRARY</title>
+   <title>All Current Items</title>
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
    <link rel="stylesheet" href="css/style.css">
-
-
+   
 </head>
-<body>
+<body id="dvd_body">
 <nav class="navbar navbar-expand-lg  navbar-dark bg-dark">
   <a class="navbar-brand" href="index.php">XTREME LIBRARY</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -22,7 +21,7 @@
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
-    <li class="nav-item active">
+    <li class="nav-item">
         <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
       </li>
 
@@ -35,19 +34,40 @@
       <li class="nav-item">
         <a class="nav-link" href="cds.php">CD's</a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item active">
         <a class="nav-link" href="dvds.php">DVD's</a>
       </li>
     </ul>
   </div>
 </nav>
 
-<div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <h1 class="display-4">BIG-EFFING-LIBRARY</h1>
-    <p class="lead">We offer a ton of non-modern types of media for you, we sell you what nobody wants anymore!</p>
-  </div>
-</div>
+
+
+
+
+
+
+<?php 
+
+$sql = "SELECT author,inv_id, auth_first_name, auth_last_name, image, publisher,publish_date, short_description, status, title, type FROM inventory WHERE type='dvd'";
+$result =  mysqli_query($connect, $sql);
+
+if ($result->num_rows > 0) {
+  while($row = $result->fetch_assoc()) {
+     echo  "
+            <p>" .$row['title']."</p>
+            <p>" .$row['auth_first_name']."</p>
+            <p>" .$row['auth_last_name']."</p>
+            <p>" .$row['publisher']."</p>
+            <p>" .$row['publish_date']."</p>
+            <p>
+            <a href='details.php?inv_id=" .$row['inv_id']."'><button type='button'>Details</button></a>" ;
+
+  }
+} else {
+   echo "No date available!";
+}
+?> 
 
 </body>
 </html>
